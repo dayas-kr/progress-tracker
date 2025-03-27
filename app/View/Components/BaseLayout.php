00b982e6bd35ffbbe951909809c93e6a7facf0bb
dayas-kr <149292309+dayas-kr@users.noreply.github.com>
2025-03-27 17:52:36 +0530

    feat: add title property to BaseLayout component and update base layout view for dynamic title rendering

--- app/View/Components/BaseLayout.php
+++ app/View/Components/BaseLayout.php
@@ -7,6 +7,18 @@ use Illuminate\View\View;
 
 class BaseLayout extends Component
 {
+    public $title;
+
+    /**
+     * Create a new component instance.
+     *
+     * @param string|null $title
+     */
+    public function __construct($title = null)
+    {
+        $this->title = $title;
+    }
+
     /**
      * Get the view / contents that represents the component.
      */
--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -6,7 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>
+        @if (isset($title))
+            {{ $title }}
+        @else
+            {{ config('app.name', 'Laravel') }}
+        @endif
+    </title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
