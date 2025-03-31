3577aaa03d2f7230703307cb16e4b8e924990dd0
dayas-kr <149292309+dayas-kr@users.noreply.github.com>
2025-03-31 12:45:44 +0530

    feat: add size prop to checkbox component for customizable dimensions

--- resources/views/components/checkbox.blade.php
+++ resources/views/components/checkbox.blade.php
@@ -5,6 +5,7 @@
     'description' => '',
     'disabled' => false,
     'checked' => false,
+    'size' => 'medium', // Added size prop with default 'medium'
 ])
 
 @php
@@ -21,6 +22,23 @@ $disabledClasses = implode(' ', [
 
 // Label text styling based on disabled state.
 $disabledLabelClasses = $disabled ? 'text-zinc-400 dark:text-zinc-600' : 'text-zinc-900 dark:text-zinc-50';
+
+// Size-based classes
+$checkboxSizes = [
+    'small' => 'w-4 h-4',
+    'medium' => 'w-5 h-5',
+    'large' => 'w-6 h-6',
+];
+
+$checkboxSize = $checkboxSizes[$size] ?? $checkboxSizes['medium'];
+
+$iconSizes = [
+    'small' => 'size-3',
+    'medium' => 'size-3.5',
+    'large' => 'size-4',
+];
+
+$iconSize = $iconSizes[$size] ?? $iconSizes['medium'];
 @endphp
 
 <div class="grid grid-cols-[auto_1fr] gap-x-2 gap-y-1 data-[disabled=true]:[&>[data-main-checkbox-label]]:opacity-50 data-[disabled=true]:[&>[data-checkbox-description]]:opacity-50"
@@ -35,9 +53,9 @@ $disabledLabelClasses = $disabled ? 'text-zinc-400 dark:text-zinc-600' : 'text-z
                   focus-visible:outline-0 focus-visible:ring-none focus-visible:[&_.custom-checkbox]:ring-2 focus-visible:[&_.custom-checkbox]:ring-blue-500
                   focus-visible:[&_.custom-checkbox]:ring-offset-2 dark:focus-visible:[&_.custom-checkbox]:ring-offset-zinc-900">
         <span
-            class="flex justify-center items-center w-5 h-5 rounded-[0.3125rem] border border-zinc-300 dark:border-zinc-700 custom-checkbox text-zinc-900 {{ $disabled ? 'bg-zinc-100 dark:bg-zinc-800' : '' }}">
+            class="flex justify-center items-center rounded-[0.3125rem] border border-zinc-300 dark:border-zinc-700 custom-checkbox text-zinc-900 {{ $checkboxSize }} {{ $disabled ? 'bg-zinc-100 dark:bg-zinc-800' : '' }}">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor"
-                class="size-3.5 duration-300 ease-out peer-checked:scale-100 scale-0 text-white dark:text-zinc-800">
+                class="{{ $iconSize }} duration-300 ease-out peer-checked:scale-100 scale-0 text-white dark:text-zinc-800">
                 <path
                     d="m382-393 321-321q20.09-20 48.55-20Q780-734 800-714.21q20 19.79 20 48.5T800-617L430-248q-19.82 20-47.91 20Q354-228 334-248L161-420q-20-20.73-20-48.87Q141-497 160.79-517q20.79-21 49-21T258-517l124 124Z" />
             </svg>
@@ -48,8 +66,6 @@ $disabledLabelClasses = $disabled ? 'text-zinc-400 dark:text-zinc-600' : 'text-z
             class="col-start-2 select-none">{{ $label }}</x-label>
     @endif
     @if ($description)
-        {{-- <p data-slot="checkbox-description" class="text-sm col-start-2 select-none text-zinc-500 dark:text-white/60">
-            {{ $description }}</p> --}}
         <x-description data-checkbox-description class="col-start-2 select-none">{{ $description }}</x-description>
     @endif
 </div>
